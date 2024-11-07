248381216e377100c2947cf99c431dae89bbbf51
selviniah <xqwyss1699@gmail.com>
2024-11-06 23:45:27 -0500

    All the projects are converted into cross-platform compatible.
    A* algorithm improved

--- AStar/Managers/PathFinder.cs
+++ AStar/Managers/PathFinder.cs
@@ -1,152 +1,173 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
 using AStar.Models;
 using Microsoft.Xna.Framework;
 
-namespace AStar.Managers;
-
-public static class PathFinder
+namespace AStar.Managers
 {
-    class Node
+    public static class PathFinder
     {
-        public readonly int X; //Relative indexes
-        public readonly int Y; //Relative indexes
-        public Node Parent;
-        public bool Visited;
-
-        //A* implementation
-        public int Hcost = 0; // Current to goal cost. We need to manually calculate HCost with using GoalX and GoalY. 
-        public int GCost = 0; // Current to Start cost. Every time we move, it means we are adding +1 to GCost for the current node. 
-        public int FCost => GCost + Hcost;
-        public static int GetHCost(int x1, int y1, int x2, int y2)
+        class Node : IComparable<Node>
         {
-            return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+            public int X { get; }
+            public int Y { get; }
+            public Node Parent { get; set; }
+            public int GCost { get; set; }
+            public int HCost { get; set; }
+            public int FCost => GCost + HCost;
+
+            public Node(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public int CompareTo(Node other)
+            {
+                int compare = FCost.CompareTo(other.FCost);
+                if (compare == 0)
+                    compare = HCost.CompareTo(other.HCost);
+                return compare;
+            }
         }
 
-        public Node(int x, int y)
+        private static Map _map;
+        private static readonly int[] dx = { -1, 1, 0, 0 }; // Left, Right, Up, Down
+        private static readonly int[] dy = { 0, 0, -1, 1 };
+
+        public static void Init(Map map)
         {
-            this.X = x;
-            this.Y = y;
+            _map = map;
         }
-    }
 
-    private static Node[,] _nodeMap;
-    private static Map _map;
-    private static readonly int[] row = { -1, 1, 0, 0 }; //  Right, Left, Up, Down
-    private static readonly int[] col = { 0, 0, -1, 1 }; // Corresponding to the rows
+        public static async void AStar(int goalX, int goalY)
+        {
+            // Clear previous path and visited tiles
+            ResetMapTiles();
 
+            var openSet = new PriorityQueue<Node, int>();
+            var cameFrom = new Dictionary<(int, int), Node>();
+            var gScore = new Dictionary<(int, int), int>();
+            var openSetHash = new HashSet<(int, int)>();
+            var closedSet = new HashSet<(int, int)>();
 
-    public static void Init(Map map)
-    {
-        _map = map;
-    }
+            (int startX, int startY) = _map.ScreenToMap(new Vector2(0, 0));
+            var startNode = new Node(startX, startY);
+            gScore[(startX, startY)] = 0;
+            startNode.HCost = Heuristic(startX, startY, goalX, goalY);
+            openSet.Enqueue(startNode, startNode.FCost);
+            openSetHash.Add((startX, startY));
 
-    private static bool IsValid(int x, int y)
-    {
-        return x >= 0 && x < _nodeMap.GetLength(0) && y >= 0 && y < _nodeMap.GetLength(1);
-    }
+            while (openSet.Count > 0)
+            {
+                var current = openSet.Dequeue();
+                openSetHash.Remove((current.X, current.Y));
+                closedSet.Add((current.X, current.Y));
 
-    private static void CreateNodeMap()
-    {
-        _nodeMap = new Node[_map.Size.X, _map.Size.Y];
+                // Mark current tile as visited for visualization
+                _map.Tiles[current.X, current.Y].Visited = true;
+                _map.Tiles[current.X, current.Y].VisitColor = Color.LightGray;
 
-        for (int x = 0; x < _map.Size.X; x++)
+                if (current.X == goalX && current.Y == goalY)
+                {
+                    RetracePath(cameFrom, current);
+                    return;
+                }
+
+                for (int i = 0; i < dx.Length; i++)
+                {
+                    int neighborX = current.X + dx[i];
+                    int neighborY = current.Y + dy[i];
+
+                    if (!IsValid(neighborX, neighborY))
+                        continue;
+
+                    if (_map.Tiles[neighborX, neighborY].Blocked)
+                        continue;
+
+                    if (closedSet.Contains((neighborX, neighborY)))
+                        continue;
+
+                    int tentativeGScore = gScore[(current.X, current.Y)] + 1;
+
+                    bool isInOpenSet = openSetHash.Contains((neighborX, neighborY));
+
+                    if (!isInOpenSet || tentativeGScore < gScore.GetValueOrDefault((neighborX, neighborY), int.MaxValue))
+                    {
+                        cameFrom[(neighborX, neighborY)] = current;
+                        gScore[(neighborX, neighborY)] = tentativeGScore;
+                        int hCost = Heuristic(neighborX, neighborY, goalX, goalY);
+                        var neighborNode = new Node(neighborX, neighborY)
+                        {
+                            Parent = current,
+                            GCost = tentativeGScore,
+                            HCost = hCost
+                        };
+
+                        if (!isInOpenSet)
+                        {
+                            openSet.Enqueue(neighborNode, neighborNode.FCost);
+                            openSetHash.Add((neighborX, neighborY));
+
+                            // For visualization: Mark the neighbor tile as being in the open set
+                            _map.Tiles[neighborX, neighborY].VisitColor = Color.DarkGray;
+                        }
+                        else
+                        {
+                            // Update the priority in the queue
+                            // Since PriorityQueue doesn't support decrease-key operation, we can re-enqueue the node
+                            openSet.Enqueue(neighborNode, neighborNode.FCost);
+                        }
+                    }
+                }
+
+                // Include delay for visualization
+                await Task.Delay(15);
+            }
+
+            // No path found
+        }
+
+        private static void ResetMapTiles()
         {
             for (int y = 0; y < _map.Size.Y; y++)
             {
-                _map.Tiles[x, y].Path = false;
-                _map.Tiles[x, y].Visited = false;
-                _nodeMap[x, y] = new Node(x, y);
-                if (_map.Tiles[x, y].Blocked)
+                for (int x = 0; x < _map.Size.X; x++)
                 {
-                    _nodeMap[x, y].Visited = true;
+                    _map.Tiles[x, y].Visited = false;
+                    _map.Tiles[x, y].Path = false;
+                    _map.Tiles[x, y].VisitColor = Color.White;
                 }
             }
         }
-    }
 
-    public static async void AStar(int goalX, int goalY)
-    {
-        CreateNodeMap();
-        List<Node> openList = new List<Node>(); // List of nodes
-        (int startX, int startY) = _map.ScreenToMap(new Vector2(0, 0)); // Start at first tile
-
-        // Determine the start, mark it visited, and calculate the H cost.
-        Node start = _nodeMap[startX, startY];
-        start.Visited = true;
-        start.Hcost = Node.GetHCost(startX, startY, goalX, goalY);
-        openList.Add(start); // We added something to the list so we can start while loop
-
-        while (openList.Count > 0)
+        private static int Heuristic(int x1, int y1, int x2, int y2)
         {
-            Node curr = openList.OrderBy(n => n.FCost).First(); // Find the lowest Fcost one inside node list
-            openList.Remove(curr);
-            if (curr.X == goalX && curr.Y == goalY) // If the path is found, retrace
+            // Manhattan distance
+            return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+        }
+
+        private static bool IsValid(int x, int y)
+        {
+            return x >= 0 && x < _map.Size.X && y >= 0 && y < _map.Size.Y;
+        }
+
+        private static void RetracePath(Dictionary<(int, int), Node> cameFrom, Node current)
+        {
+            var path = new List<Node>();
+            while (cameFrom.ContainsKey((current.X, current.Y)))
             {
-                RetracePath(goalX, goalY);
-                return;
+                path.Add(current);
+                current = cameFrom[(current.X, current.Y)];
             }
+            path.Add(current);
+            path.Reverse();
 
-            for (int i = 0; i < row.Length; i++)
+            foreach (var node in path)
             {
-                int newX = curr.X + row[i];
-                int newY = curr.Y + col[i];
-
-                if (IsValid(newX, newY) && !_nodeMap[newX, newY].Visited) // Valid and not visited?
-                {
-                    Node neighbour = _nodeMap[newX, newY];
-                    int turnPenalty = 5;
-
-                    // Calculate the turn penalty if there is a parent and the direction changes
-                    if (curr.Parent != null && (curr.X - curr.Parent.X != row[i] || curr.Y - curr.Parent.Y != col[i]))
-                    {
-                        turnPenalty = 5; // Arbitrary penalty for turning
-                    }
-                    
-                    int costToMove = curr.GCost + 1 + turnPenalty; // Moving costs +1, and turning might add penalty
-
-                    // If costToMove is less than the neighbour's GCost or the neighbour is not in openList
-                    if (costToMove < neighbour.GCost || !openList.Contains(neighbour))
-                    {
-                        neighbour.GCost = costToMove;
-                        neighbour.Hcost = Node.GetHCost(newX, newY, goalX, goalY);
-                        neighbour.Parent = curr;
-                        _map.Tiles[newX, newY].VisitColor = Color.DarkGray;
-                        openList.Add(neighbour);
-                        _nodeMap[newX, newY].Visited = true;
-                        _map.Tiles[newX, newY].Visited = true;
-                    }
-                }
+                _map.Tiles[node.X, node.Y].Path = true;
             }
-
         }
-
-        // If nothing is found, return
-        return;
-    }
-
-
-    private static List<Vector2> RetracePath(int goalX, int goalY)
-    {
-        Stack<Vector2> stack = new Stack<Vector2>();
-        List<Vector2> result = new List<Vector2>();
-        Node curr = _nodeMap[goalX, goalY];
-
-        while (curr is not null) //retrace back with parent like a chain going end to start. That's why we using stack. Because we going from end to start. 
-        {
-            _map.Tiles[curr.X, curr.Y].Path = true;
-            stack.Push(_map.Tiles[curr.X, curr.Y].Position); //push to current mao's position to stack. Those will be path locations. 
-            curr = curr.Parent;
-        }
-
-        while (stack.Count > 0) //reverse the stack and add to result
-        {
-            result.Add(stack.Pop());
-        }
-
-        return result;
     }
 }
